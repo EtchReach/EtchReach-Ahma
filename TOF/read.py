7a32b0668312bde3d5ecf5cb6c8760670a455dd1
chayhuixiang <chayhuixiang@gmail.com>
2022-11-15 22:20:04 +0800

    Updated speed calculation

--- TOF/read.py
+++ TOF/read.py
@@ -31,9 +31,9 @@ tof.open()
 # and inter-measurement time in milliseconds.
 # If you uncomment the line below to set a budget you
 # should use `tof.start_ranging(0)`
-# tof.set_timing(66000, 70)
+tof.set_timing(66000, 70)
 
-tof.start_ranging(1)  # Start ranging
+tof.start_ranging(3)  # Start ranging
                       # 0 = Unchanged
                       # 1 = Short Range
                       # 2 = Medium Range
@@ -52,8 +52,31 @@ def exit_handler(signal, frame):
 
 # Attach a signal handler to catch SIGINT (Ctrl+C) and exit gracefully
 signal.signal(signal.SIGINT, exit_handler)
+read_delay = 1
+prev_distance_in_mm = 0
+speed_arr = []
 
 while running:
     distance_in_mm = tof.get_distance()
+    if not prev_distance_in_mm:
+        prev_distance_in_mm = distance_in_mm
+    
+    speed = (distance_in_mm - prev_distance_in_mm)/read_delay
+    speed_arr.append(speed)
+    len_speed_arr = len(speed_arr)
+    
+    avg_value = 10
+    
+    speed_arr = speed_arr[-avg_value:] if len_speed_arr > 10 else speed_arr
+    
     print("Distance: {}mm".format(distance_in_mm))
-    time.sleep(0.1)
+    print(f"Current speed: {speed}mm/s")
+    print(f"Speed Arr: {speed_arr}")
+    print(f"Average speed: {sum(speed_arr)/len(speed_arr)}mm/s")
+    
+    
+    prev_distance_in_mm = distance_in_mm
+    
+    time.sleep(read_delay)
+    
+
