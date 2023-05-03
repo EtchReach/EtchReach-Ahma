22eee4306f32aaf15e8cdf8097ae36452c7b5b6f
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-05-03 21:45:57 +0800

    Fix Networking

--- bike_detector/lib/utils/networking.dart
+++ bike_detector/lib/utils/networking.dart
@@ -4,12 +4,16 @@ import 'package:http/http.dart' as http;
 
 class Networking {
   static Future<Uint8List?> fetchImage() async {
-    final response = await http.get(Uri.parse('http://192.168.31.131/capture'));
-    print(response.bodyBytes);
-    if (response.statusCode == 200) {
-      return response.bodyBytes;
-    } else {
-      return null;
+    try {
+      final response =
+          await http.get(Uri.parse('http://192.168.31.131/capture'));
+      print(response.bodyBytes);
+      if (response.statusCode == 200) {
+        return response.bodyBytes;
+      }
+    } catch (e) {
+      print('$e');
     }
+    return null;
   }
 }
