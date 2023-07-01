404d9c1905e568449625a464929cf1acdcf8b7dd
ActualNugget <isvaran@tuta.io>
2023-07-01 18:38:05 +0800

    added tflite_flutter_helper deps

--- bike_detector/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bike_detector/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import connectivity_plus
 import path_provider_foundation
+import tflite_flutter_helper
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  TfliteFlutterHelperPlugin.register(with: registry.registrar(forPlugin: "TfliteFlutterHelperPlugin"))
 }
